d441b850bc614ed65027a7bba7c42df8d9b49977
Guillaume RICHARD 8 <36263394+guillaumeRichard8@users.noreply.github.com>
2026-06-24 00:26:20 +0200

    Add files via upload
    Files

---
+++ ChildFrm.cpp
@@ -0,0 +1,67 @@
+
+// ChildFrm.cpp : implémentation de la classe CChildFrame
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MyFractal.h"
+
+#include "ChildFrm.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CChildFrame
+
+IMPLEMENT_DYNCREATE(CChildFrame, CMDIChildWndEx)
+
+BEGIN_MESSAGE_MAP(CChildFrame, CMDIChildWndEx)
+END_MESSAGE_MAP()
+
+// construction/destruction de CChildFrame
+
+CChildFrame::CChildFrame() noexcept
+{
+	// TODO: ajoutez ici le code d'une initialisation de membre
+}
+
+CChildFrame::~CChildFrame()
+{
+}
+
+BOOL CChildFrame::OnCreateClient(LPCREATESTRUCT /*lpcs*/, CCreateContext* pContext)
+{
+	return m_wndSplitter.Create(this,
+		2, 2,			// TODO: ajustez le nombre de lignes, de colonnes
+		CSize(10, 10),	// TODO: ajustez la taille minimale du volet
+		pContext);
+}
+
+BOOL CChildFrame::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: changez ici la classe ou les styles Window en modifiant CREATESTRUCT cs
+	if( !CMDIChildWndEx::PreCreateWindow(cs) )
+		return FALSE;
+
+	cs.style = WS_CHILD | WS_VISIBLE | WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU
+		| FWS_ADDTOTITLE | WS_THICKFRAME;
+
+	return TRUE;
+}
+
+// diagnostics de CChildFrame
+
+#ifdef _DEBUG
+void CChildFrame::AssertValid() const
+{
+	CMDIChildWndEx::AssertValid();
+}
+
+void CChildFrame::Dump(CDumpContext& dc) const
+{
+	CMDIChildWndEx::Dump(dc);
+}
+#endif //_DEBUG
+
+// gestionnaires de messages de CChildFrame
